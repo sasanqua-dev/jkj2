fab5161a98b0ee6495050dc51c2bda05be03c399
jikken-j05 <user@jikken-j05.pc>
2026-04-15 17:15:37 +0900

    fix

--- test_photo_sensor/test_photo_sensor.ino
+++ test_photo_sensor/test_photo_sensor.ino
@@ -1,13 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(11, OUTPUT);
-  pinMode(A1, OUTPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(11, HIGH);   // LEDを点灯(HIGHは電圧レベルを5Vにする)    // LEDを消灯(LOWは電圧レベルを0Vにする)                   // 1秒間(1000ms)待つ
-  int val = analogRead(A0);
+     // LEDを点灯(HIGHは電圧レベルを5Vにする)    // LEDを消灯(LOWは電圧レベルを0Vにする)                   // 1秒間(1000ms)待つ
+  int val = analogRead(A3);
   analogWrite(11, 150);
   Serial.println(val);
+  if (val < 500) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  delay(100);
 }
