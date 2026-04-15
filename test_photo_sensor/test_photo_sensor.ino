1b6622363d6032f758730670f2787510dad0ab6a
jikken-j05 <user@jikken-j05.pc>
2026-04-15 13:18:54 +0900

    test prog

---
+++ test_photo_sensor/test_photo_sensor.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // LEDを点灯(HIGHは電圧レベルを5Vにする)    // LEDを消灯(LOWは電圧レベルを0Vにする)                   // 1秒間(1000ms)待つ
+  int val = analogRead(A0);
+  Serial.println(val);
+}
