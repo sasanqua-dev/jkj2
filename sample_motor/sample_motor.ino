13c55739e0dc9414411aad900e81748a5c30fb89
jikken-j05 <user@jikken-j05.pc>
2026-04-22 15:56:01 +0900

    fix

--- sample_motor/sample_motor.ino
+++ sample_motor/sample_motor.ino
@@ -6,6 +6,8 @@ const int pwm_motor_r = 10; // PWM信号生成可能なピンを選ぶ
 const int motor_l1 = 4;
 const int motor_l2 = 5;
 const int pwm_motor_l = 11;
+
+int last_turn_flag = 0; // 1 = 右 2 = 左
 // フォトリフレクタ
 const int sensor = A3;
 // センサから読み取った値（analog）
@@ -48,30 +50,50 @@ void backward(int speedBase = 200) { // 後退させる関数
   analogWrite(pwm_motor_r, speedBase);
 }
 
-void turnLeft(int speedBase = 200) { // 左に曲がる関数
+void turnLeft(int speedBase = 150) { // 左に曲がる関数
   digitalWrite(motor_l1, LOW);
   digitalWrite(motor_l2, HIGH);
   analogWrite(pwm_motor_l, speedBase + 25);
   digitalWrite(motor_r1, HIGH);
   digitalWrite(motor_r2, LOW);
-  analogWrite(pwm_motor_r, speedBase - 25);
+  analogWrite(pwm_motor_r, speedBase);
 }
 
-void turnRight(int speedBase = 200) { // 右に曲がる関数
+void turnRight(int speedBase = 150) { // 右に曲がる関数
   digitalWrite(motor_l1, HIGH);
   digitalWrite(motor_l2, LOW);
-  analogWrite(pwm_motor_l, speedBase - 25);
+  analogWrite(pwm_motor_l, speedBase);
   digitalWrite(motor_r1, LOW);
   digitalWrite(motor_r2, HIGH);
   analogWrite(pwm_motor_r, speedBase + 25);
 }
 
 void loop() { // 制御プログラム
+  delay(500);
   // センサの値をアナログ値で読み取る
   val = analogRead(sensor);
   // シリアルポートに読み取った値を表示させる
   Serial.println(val); // 表示して改行
   // ここから制御分岐
-  forward(255);
-  delay(20); // 0.02 秒待つ（動作を続ける）
+  if (val > 600) {
+    backward(255);
+    last_turn_flag = 0;
+    return;
+  }
+  if (last_turn_flag == 0) {
+    turnLeft(50);
+    last_turn_flag = 2;
+    return;
+  }
+  if (last_turn_flag == 1) {
+    turnLeft(50);
+    last_turn_flag = 2;
+    return;
+  }
+  if (last_turn_flag == 2) {
+    turnRight(50);
+    last_turn_flag = 1;
+    return;
+  }
+  delay(500); // 0.02 秒待つ（動作を続ける）
 }
